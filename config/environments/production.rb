f9121060ce90a2920e6de1c242028860ccaa70a5
Duke Jones <duke@gopago.com>
2013-11-17 03:42:17 -0800

    Include banner SVG in the assets precompile.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Portal::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( Bauhausl.ttl Bauhausm.ttf BauhausStd-Light.otf BauhausStd-Medium.otf HelveticaLTStd-Light.otf HelveticaLTStd-LightObl.otf Sensation_Light.ttf )
+  config.assets.precompile += %w( Bauhausl.ttl Bauhausm.ttf BauhausStd-Light.otf BauhausStd-Medium.otf HelveticaLTStd-Light.otf HelveticaLTStd-LightObl.otf Sensation_Light.ttf banner-vector-overlay.svg)
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

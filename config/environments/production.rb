40e0ae254bc48e7cb0dd41dedaab09513e5647e8
Duke Jones <duke@gopago.com>
2013-11-17 23:56:26 -0800

    splash page vector overlay.  rework responsiveness of splash page.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,8 +60,8 @@ Portal::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( Bauhausl.ttl Bauhausm.ttf BauhausStd-Light.otf BauhausStd-Medium.otf HelveticaLTStd-Light.otf HelveticaLTStd-LightObl.otf Sensation_Light.ttf banner-vector-overlay.svg)
-  
+  config.assets.precompile += %w( Bauhausl.ttl Bauhausm.ttf BauhausStd-Light.otf BauhausStd-Medium.otf HelveticaLTStd-Light.otf HelveticaLTStd-LightObl.otf Sensation_Light.ttf )
+  config.assets.precompile += %w( banner-vector-overlay.svg splash-vector-overlay.svg )
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false

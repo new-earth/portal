a4c62498ea71750ddc93d040a01bf45322f6bf5d
Joe Black <joe@spektrumtheory.com>
2013-11-18 21:13:46 -0800

    Retooled Top Nav, added google web font loader, added google webfont: Latos, created login modal, misc ui tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def init_static_assets(assets='base')
+
+    case assets
+    
+    when 'base'
+      render :partial => "shared/base_assets"
+    end
+    
+  end
+
 end
+

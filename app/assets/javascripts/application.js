8d3dbaa650f30b48ffccaebb9749e5962fab9de0
Joe Black <joe@spektrumtheory.com>
2013-11-21 18:22:20 -0800

    fixed an accident where i saved the wrong version of the login modal haml, added some inline documentation for javascript and css, cleaned up a few errors I found with duplicated content in some of my sass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,10 +21,18 @@ window.App = {
   Views: {}
 }
 $(document).ready(function() {
+
+  // When mobile size menu link clicked, slide toggle desktop-nav content  
   $("#main-menu .mobile-nav a").click(function(e) { 
     e.preventDefault(); 
     $("#main-menu .desktop-nav").slideToggle(); 
   });
+
+  /* 
+   * FIX: Fixes bug where resizing nav to smallest size then toggling open and close on the desktop-nav 
+   * content, then resizing to next largest breaktop, nav disappears. toggle is setting display inline 
+   * which will override the selector weight of the media query. 
+   */
   $(window).resize(function () {
     var width = $(window).width();
     if (width > 1049) {
@@ -32,7 +40,13 @@ $(document).ready(function() {
     } 
   });
 
+  /* 
+   * Autofocus the email field when the login modal is displayed. 
+   * Note: I've used a recyclable class of autofocus on the field to autofocus, so this is easily 
+   * extendable in the future.
+   */
   $('#loginModal').on('shown.bs.modal', function () {
     $('#loginModal .autofocus').focus();
-})
+  })
+  
 });

b7b1689dc38273aa22bc4fefc04bd6a21b78905d
Duke Jones <duke@gopago.com>
2013-11-27 08:33:35 -0800

    Update the id of the body based on what section we are on.  Delete extraneous files, remove extraneous routes.  Route everything through PagesController.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,25 @@
 class PagesController < ApplicationController
+  before_filter :set_section
+
   def enter_new_earth
     
+  end
+
+  def institute
+    
+  end
+
+  def bank_exchange
+    
+  end
+
+  def festival
+    
+  end
+
+  protected
+
+  def set_section
+    @section = params[:section]
   end
 end

7cd7d0a74969935bbf82dae2cfbe4151fbfd067a
Duke Jones <duke@gopago.com>
2013-11-30 23:08:26 -0800

    Add new vector images.  Use them.  Requires some UI tweaking for it to look correct.

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :log_level, :debug
 set :pty, true
 
 set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system vendor/assets/components}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Portal::Application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -61,7 +61,7 @@ Portal::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += %w( Bauhausl.ttl Bauhausm.ttf BauhausStd-Light.otf BauhausStd-Medium.otf HelveticaLTStd-Light.otf HelveticaLTStd-LightObl.otf Sensation_Light.ttf )
-  config.assets.precompile += %w( banner-vector-overlay.svg splash-vector-overlay.svg )
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false

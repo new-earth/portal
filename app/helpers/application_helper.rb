cb4aa8880b432fb981cb3934aecf298f903c1ec6
Joe Black <joe@spektrumtheory.com>
2013-12-02 01:00:21 -0800

    Added support for breadcrumbs, see additions to controllers. Added a few lines to pages controller to set current action name titlized as an instance variable and feed this into breadcrumbs. Created application level helper render_breadcrumbs to output the custom breadcrumb rendering code that has been modified to comply with bootstrap design patterns. Tweaked default breadcrumb styles. Added microformats to breadcrumb trail. Styled breadcrumbs to better fit in with design. Added body support for scrollspy. Added section-name to body class in addition to the previous id. Using class in this way allows for multiple values to be supplied and can greatly effect the flexibility in weighting our selectors in the cascade. Added a new svg icon to assets/images/icons called fallback-circle. Probably other stuff too. :D

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,14 @@
 module ApplicationHelper
 
   def init_static_assets(assets='base')
-
     case assets
-    
     when 'base'
       render :partial => "shared/base_assets"
-    end
-    
+    end  
+  end
+
+  def render_breadcrumbs
+    render_crumbs format: :html_list, :ul_class => 'breadcrumb', :last_class => 'active', :microdata => true
   end
 
 end

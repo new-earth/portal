1aa438f95103a22f98ba2194742776c06754747a
Duke Dorje <duke.dorje@gmail.com>
2013-12-19 15:43:49 -0800

    Change mail host, so password reset emails, etc. will work.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Portal::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.default_url_options = {host: 'new-earth-project.org'}
+  config.action_mailer.default_url_options = {host: 'portal.new-earth-project.org'}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
